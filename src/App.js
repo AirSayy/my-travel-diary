67c5af237f9d7c8db7b7559b82a8a821314001c6
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-03-23 16:47:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Cards from '../src/Cards'
+import Nav from '../src/Nav'
+function App() {
+  return (
+    <>
+    <Nav />
+    <Cards />
+    </>
+    
+  );
+}
+
+export default App;
