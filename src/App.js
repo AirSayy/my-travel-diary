361a13b2e30ea4c3c3ee8e9ad2de48acb646fe9c
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-03-23 17:17:18 +0100

    Created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import React from 'react'
 import Cards from '../src/Cards'
 import Nav from '../src/Nav'
+import dataApi from '../src/dataApi'
 function App() {
+  const dataElements = dataApi.map(data => {
+    return (
+      <Cards />
+    )
+  })
   return (
     <>
     <Nav />
-    <Cards />
+    {dataElemets}
     </>
     
   );
