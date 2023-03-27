c8b4fcf77da6336f1f29e75aa0acbcc0ae37d9af
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-03-27 12:53:48 +0100

    added star image

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,11 +4,14 @@ function Cards(props) {
   console.log(props)
   return (
     <div className='card mt-10 flex flex-col items-center md:flex-row md:max-w-xl'>
-      <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`../src/images/${props.data.image}`} alt = "location images" />
+      <div>
+      <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`../images/${props.data.image}`} alt = "location images" />
+      </div>
+      
       <div>
         
         <div  class=" flex flex-row ml-5">
-        <img src="../images/star.png" className="card--star" />
+        <img src= "./images/star.png" alt = "star" className="card--star" />
         <span class="mb-90 text-.5xl font-bold text-black-500 dark:text-gray-400 mr-5">{props.data.location}</span>
         <a class="mb-90 text-.5xl font-small text-gray-500 dark:text-gray-400" href={props.data.googleMapUrl} target='_blank' rel="noreferrer" >Google Map</a>
         </div>
