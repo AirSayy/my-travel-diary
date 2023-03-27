ce30b79c533852fa5b542112cb46bdb1198c04a6
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-03-27 12:37:12 +0100

    added images and google links

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,14 +3,14 @@ import React from 'react'
 function Cards(props) {
   console.log(props)
   return (
-    <div className='card mt-10 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
-      <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={"../{data.props.image}"} />
+    <div className='card mt-10 flex flex-col items-center md:flex-row md:max-w-xl'>
+      <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`../src/images/${props.data.image}`} alt = "location images" />
       <div>
         
         <div  class=" flex flex-row ml-5">
         <img src="../images/star.png" className="card--star" />
         <span class="mb-90 text-.5xl font-bold text-black-500 dark:text-gray-400 mr-5">{props.data.location}</span>
-        <span class="mb-90 text-.5xl font-small text-gray-500 dark:text-gray-400">{props.data.googleMapUrl}</span>
+        <a class="mb-90 text-.5xl font-small text-gray-500 dark:text-gray-400" href={props.data.googleMapUrl} target='_blank' rel="noreferrer" >Google Map</a>
         </div>
         
         <div  class=" flex flex-col justify-between p-4 leading-normal">
--- src/dataApi.js
+++ src/dataApi.js
@@ -1,22 +1,32 @@
 export default [
     {
         id : 1,
-        title: "Mount Fuji",
-        location : "Asia",
-        googleMapUrl : "map",
+        title: "Yankari game reserve",
+        location : "Bauchi",
+        googleMapUrl : "https://goo.gl/maps/pcwcipvVXPRPL8fc9",
         startDate : "12 Jan,2022",
         endDate : "24 Jan, 2022",
-        description: "Asia is the largest and most populous continent in the world. It is home to some of the oldest civilisations, and some of the most diverse cultures, languages, and religions. It is a continent of great economic and cultural importance.",
-        image : "image"
+        description: "Yankari Game Reserve is a large wildlife park and former National Park located in the south-central part of Bauchi State, in northeastern Nigeria.",
+        image : "yankari.jpeg"
     },
     {
         id : 2,
-        title: "Mount Fuji",
-        location : "Asia",
-        googleMapUrl : "map",
+        title: "Ake",
+        location : "Abeokuta",
+        googleMapUrl : "https://goo.gl/maps/CpDXnC4zqTYNeSsy5",
         startDate : "12 Jan,2022",
         endDate : "24 Jan, 2022",
-        description: "Asia is the largest and most populous continent in the world. It is home to some of the oldest civilisations, and some of the most diverse cultures, languages, and religions. It is a continent of great economic and cultural importance.",
-        image : "image"
+        description: "Abeokuta was a walled town, and relics of the old wall still exist. Notable buildings include the Ake (the residence of the alake), Centenary Hall (1930), and several churches and mosques.",
+        image : "abeokuta.jpeg"
+    },
+    {
+        id : 3,
+        title: "Capital city",
+        location : "Abuja",
+        googleMapUrl : "https://goo.gl/maps/7S4hCa7RXJkvjhhZ8",
+        startDate : "12 Jan,2022",
+        endDate : "24 Jan, 2022",
+        description: "Abuja is known for being one of the few purpose-built capital cities in Africa, as well as being one of the wealthiest. Abuja is Nigeria's administrative and political capital. ",
+        image : "abuja.jpeg"
     },
 ]
